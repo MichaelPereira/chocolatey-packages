9bd19b0d06891b0fe16806957b544305a8539253
Nick Stolwijk <nick.stolwijk@rigd-loxia.nl>
2019-11-29 14:39:33 +0100

    Update to Maven 3.6.3.

--- Maven/tools/chocolateyInstall.ps1
+++ Maven/tools/chocolateyInstall.ps1
@@ -5,7 +5,8 @@ function CreateFolder ([string]$Path) {
   New-Item -Path $Path -type directory -Force
 }
 
-$version = '3.6.2'
+$version = '3.6.3'
+$checksum = "1c095ed556eda06c6d82fdf52200bc4f3437a1bab42387e801d6f4c56e833fb82b16e8bf0aab95c9708de7bfb55ec27f653a7cf0f491acebc541af234eded94d"
 $name = "apache-maven-$version"
 $tools = Split-Path $MyInvocation.MyCommand.Definition
 $package = Split-Path $tools
@@ -16,8 +17,6 @@ $pathToAdd = Join-Path '%M2_HOME%' 'bin'
 $url = "https://archive.apache.org/dist/maven/maven-3/$version/binaries/$name-bin.zip"
 
 # Delete leftovers from previous versions
-# TODO: moved to chocolateyBeforeModify.ps1 on v3.6.2. Remove from next release
-Remove-Item "$(Join-Path $package 'apache-maven-*')" -Force -Recurse
 $pathToRemove = Join-Path '%M2_HOME%' 'bin'
 
 try {
@@ -40,7 +39,7 @@ finally {
 Install-ChocolateyZipPackage `
     -PackageName 'Maven' `
     -Url $url `
-    -Checksum '4bb0e0bb1fb74f1b990ba9a6493cc6345873d9188fc7613df16ab0d5bd2017de5a3917af4502792f0bad1fcc95785dcc6660f7add53548e0ec4bfb30ce4b1da7' `
+    -Checksum $checksum `
     -ChecksumType 'sha512' `
     -UnzipLocation $package
 
