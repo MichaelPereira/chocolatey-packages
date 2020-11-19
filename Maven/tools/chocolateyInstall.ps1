4cf80f352dd18b3decbe6973acd08097a3552a05
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2020-11-18 20:29:14 -0600

    Maven initial refactor scripts

--- Maven/tools/chocolateyInstall.ps1
+++ Maven/tools/chocolateyInstall.ps1
@@ -7,8 +7,14 @@ $package = Split-Path $toolsDir
 $m2_home = Join-Path $package $name
 $m2_repo = Join-Path $env:USERPROFILE '.m2'
 $pathToAdd = Join-Path '%M2_HOME%' 'bin'
+. $toolsDir\helpers.ps1
 
 New-Item -Path $m2_repo -Type directory -Force
 
+# Clean Environment variables 
+Uninstall-M2PathFromRegistry
+Uninstall-M2EnvVar
+
+# Add back environment variables
 [Environment]::SetEnvironmentVariable('M2_HOME', $m2_home, 'Machine')
 Install-ChocolateyPath -PathToInstall $pathToAdd -PathType 'Machine'
